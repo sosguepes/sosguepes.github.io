b84135dee61cc6f793d54dda818dbae1c7ba5a34
Antoine Eudes <antoine.eudes@gmail.com>
2023-12-07 08:15:44 +0800

    chore: remove useless code

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -7,9 +7,8 @@ import {
   ImageContainer,
   TextTitle,
 } from "./Banner.style";
-import { StaticImage, getImageData, GatsbyImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image";
 import CallButton from "components/CallButton";
-import { graphql, useStaticQuery } from "gatsby";
 
 const Banner = () => (
   <Section>
@@ -34,40 +33,5 @@ const Banner = () => (
     </Container>
   </Section>
 );
-
-// const Banner = () => {
-//   const image = useStaticQuery(graphql`
-//     query BannerImage {
-//       file(relativePath: { eq: "beekeeper-black.png" }) {
-//         id
-//         childImageSharp {
-//           gatsbyImageData(width: 540, placeholder: BLURRED)
-//         }
-//       }
-//     }
-//   `);
-//   return (
-//     <Section>
-//       <Container>
-//         <Seperator>
-//           <ImageContainer>
-//             <GatsbyImage
-//               image={image.file.childImageSharp.gatsbyImageData}
-//               alt="Beepkeeper with wasp hive"
-//             />
-//           </ImageContainer>
-//           <TextContainer>
-//             <TextTitle>Débarrassez-vous de vos nuisibles</TextTitle>
-//             <p>
-//               SOS Guêpes 74 opèrent dans toute la Haute-Savoie. Appelez Ghislain
-//               pour obtenir un devis!
-//             </p>
-//             <CallButton />
-//           </TextContainer>
-//         </Seperator>
-//       </Container>
-//     </Section>
-//   );
-// };
 
 export default Banner;
