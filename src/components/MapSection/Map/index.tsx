fb31013e873d3d9ac6b697aa963e8759c9d3bd45
Antoine Eudes <antoine.eudes@gmail.com>
2024-05-26 19:51:01 +0800

    feat: update map

--- src/components/MapSection/Map/index.tsx
+++ src/components/MapSection/Map/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export const Map = () => {
-  const position = [51.505, -0.09];
   return (
     <iframe
-      src="https://www.google.com/maps/d/u/0/embed?mid=14Q9-qQD8qHD2W4wuaWHBTjGHmBElbxk&ehbc=2E312F&noprof=1"
+      src="https://www.google.com/maps/d/embed?mid=1hl8MSwVwgxuycpJJ2eDdSpW_mseodgM&ehbc=2E312F&noprof=1"
       width="640"
       height="480"
     ></iframe>
