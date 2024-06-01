2fe555bc189f4fa482cccd2a01dfad2d828d73a1
Antoine Eudes <antoine.eudes@gmail.com>
2024-06-01 10:06:12 +0800

    feat: enable screen size evaluation for server-side rendered ssite

--- src/components/MapSection/Map/index.tsx
+++ src/components/MapSection/Map/index.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const useWindowDimensions = () => {
+  const hasWindow = typeof window !== "undefined";
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : null;
+    const height = hasWindow ? window.innerHeight : null;
+    return {
+      width,
+      height,
+    };
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    if (hasWindow) {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, [hasWindow]);
+
+  return windowDimensions;
+};
 
 export const Map = () => {
-  const mapWidth = Math.min(640, window.innerWidth - 64);
+  const { width } = useWindowDimensions();
+  let mapWidth = 640;
+  if (width) {
+    mapWidth = Math.min(640, width - 64);
+  }
   const mapHeight = (mapWidth * 2) / 3;
   return (
     <iframe
