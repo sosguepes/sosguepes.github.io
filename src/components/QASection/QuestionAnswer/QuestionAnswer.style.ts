bb8447588e14892c2471d895e010b07b4afb1215
Antoine Eudes <antoine.eudes@gmail.com>
2024-06-03 17:51:14 +0800

    feat: improve display of qa section

--- src/components/QASection/QuestionAnswer/QuestionAnswer.style.ts
+++ src/components/QASection/QuestionAnswer/QuestionAnswer.style.ts
@@ -1,23 +1,34 @@
-import { StaticImage } from "gatsby-plugin-image";
 import styled from "styled-components";
 
 export const Container = styled.div`
   border: 1px solid #d3d3d3;
-  border-radius: 10px;
   width: 100%;
   text-align: justify;
+  overflow: hidden;
+
+  &:last-child {
+    border-radius: 0px 0px 10px 10px;
+  }
 
   &:not(:last-child) {
-    margin-bottom: 8px;
+    border-bottom: none;
+  }
+
+  &:first-child {
+    border-radius: 10px 10px 0px 0px;
   }
 `;
 
 export const QuestionContainer = styled.div`
-  background-color: #eeeeee;
+  background-color: #f5f5f5;
   padding: 16px 32px 16px 32px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  &:hover {
+    background-color: #eeeeee;
+  }
 `;
 
 export const ExpandedContainer = styled.div`
@@ -27,6 +38,10 @@ export const ExpandedContainer = styled.div`
 export const Answer = styled.div`
   background-color: white;
   padding: 0px 16px;
+  margin: 0px 16px;
+  &:last-child {
+    border-radius: 0px 0px 10px 10px;
+  }
 `;
 
 export const Question = styled.div`
