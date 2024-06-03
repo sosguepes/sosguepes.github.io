93aa699053846ecc64ad52238e1c16baa778e2de
Antoine Eudes <antoine.eudes@gmail.com>
2024-06-03 19:01:40 +0800

    feat: improve display of map

--- src/components/MapSection/Map/Map.style.ts
+++ src/components/MapSection/Map/Map.style.ts
@@ -1,13 +1,16 @@
+import { MapContainer } from "react-leaflet";
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  overflow: hidden;
   width: 100%;
   @media screen and (min-width: 40em) {
     width: calc(50% - 25px);
   }
-  height: 233px;
+  height: 300px;
+`;
+
+export const StyledMapContainer = styled(MapContainer)`
+  width: 100%;
+  height: 100%;
 `;
--- src/components/MapSection/Map/index.tsx
+++ src/components/MapSection/Map/index.tsx
@@ -1,7 +1,7 @@
 import { useWindowDimensions } from "hooks/useWindowDimensions";
 import React from "react";
-import { Wrapper } from "./Map.style";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { StyledMapContainer, Wrapper } from "./Map.style";
+import { TileLayer } from "react-leaflet";
 import { Polygon } from "react-leaflet";
 import { INTERVENTION_AREA } from "./interventionArea";
 
@@ -15,8 +15,7 @@ export const Map = () => {
   const mapHeight = (mapWidth * 4) / 5;
   return (
     <Wrapper>
-      <MapContainer
-        style={{ height: mapHeight, width: mapWidth }}
+      <StyledMapContainer
         center={[46.239512, 6.512384]}
         zoom={8}
         scrollWheelZoom={true}
@@ -26,7 +25,7 @@ export const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-      </MapContainer>
+      </StyledMapContainer>
     </Wrapper>
   );
 };
