aaa9a3ac57ce3a237d6ef7ed38a4a060f99712eb
Antoine Eudes <antoine.eudes@gmail.com>
2025-03-02 16:01:46 +0800

    chore: update domain extension

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: "sosguepes",
-    siteUrl: "https://sosguepes74.com/",
+    siteUrl: "https://sosguepes74.fr/",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
