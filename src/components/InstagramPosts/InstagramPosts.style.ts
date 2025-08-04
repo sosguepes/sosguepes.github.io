7787fe8ac654aa0a8fce960cf01bedd8e15468d3
Antoine Eudes <antoine.eudes@gmail.com>
2025-08-04 23:04:40 +0800

    feat: add instagram posts

---
+++ src/components/InstagramPosts/InstagramPosts.style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const Post = styled.div`
+  margin: 0px 10px 0px 10px;
+`;
